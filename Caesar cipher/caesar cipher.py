00b2fbf98bc6404cb0f5dc84e9d08384108e5133
Martin Lund <the_tribe2003@hotmail.com>
2018-02-10 14:47:48 +0100

    Added Caesar cipher

---
+++ Caesar cipher/caesar cipher.py
@@ -0,0 +1,52 @@
+alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def get_mode():
+    while 1:
+        text = get_text()
+        key = get_key()
+        mode = set_mode()
+        if mode == "encrypt":
+            encrypt(text, key)
+        elif mode == "decrypt":
+            decrypt(text, key)
+        elif mode == "bruteforce":
+            print("brute force")
+        else:
+            print("You need to select a method")
+
+def get_text():
+    text = input("Enter cipher text here: ")
+    return text.upper()
+
+
+def get_key():
+    key = int(input("Enter key here: "))
+    return key
+
+
+def set_mode():
+    mode = input("Enter mode: encrypt, decrypt, bruteforce: ")
+    return mode.lower()
+
+
+def encrypt(text, key):
+    return cipher(text, key)
+
+
+def decrypt(text, key):
+    return cipher(text, key)
+
+
+def cipher(text, key):
+    result = ""
+    for chars in text:
+        if chars in alfabet:
+            pos = alfabet.index(chars)
+            pos = pos + key
+            pos = (pos + len(alfabet)) % len(alfabet)
+            result += alfabet[pos]
+        else:
+            result = result + chars
+    print(result)
+
+get_mode()
